06876f6f05ee17e42f16575446396f8bdb72ef46
EmanuelTejada032 <emanueltejada31295@gmail.com>
2021-08-27 10:41:44 -0400

    Add basic parent/child  routing and lazyload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = []
+const routes: Routes = [
+  {
+    path:'template',
+    loadChildren: () => import('./template/template.module').then( module => module.TemplateModule)
+  },
+  {
+    path:'reactive',
+    loadChildren: () => import('./reactive/reactive.module').then( module => module.ReactiveModule)
+  },
+]
 
 
 @NgModule({
