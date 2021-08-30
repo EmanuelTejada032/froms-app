707e99f42cc0368a0d2a869fb939ef5580259cdd
EmanuelTejada032 <emanueltejada31295@gmail.com>
2021-08-30 09:11:43 -0400

    ViewChild decorator and simple validation on price field

--- src/app/template/basics/basics.component.ts
+++ src/app/template/basics/basics.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basics',
@@ -6,16 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-
 export class BasicsComponent implements OnInit {
+
+  @ViewChild('myForm') myForm!: NgForm; //get the element called #myForm with a local reference from the html
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  sendProductToSave(formData: any){
-    console.log(formData.value)
+  validateNameField(){
+    return this.myForm?.controls.name?.invalid && this.myForm?.controls.name?.touched
+  }
+  validatePriceField(){
+    return this.myForm?.controls.price?.invalid && this.myForm?.controls.price?.touched
   }
 
+  sendProductToSave(){
+    console.log(this.myForm)
+    console.log(this.myForm.form.value)
+  }
 }
