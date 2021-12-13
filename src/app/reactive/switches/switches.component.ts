cdd835477b9a6b35ad0e538e59106b5a12a84822
Emanuel De Jesús Tejada Coste <e.coste@sisalril.gov.do>
2021-12-13 10:26:54 -0400

    Add reactive forms switches , add on value changes triggers to the form

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
+enum genders {
+  male= "M",
+  female="F"
+}
 
 @Component({
   selector: 'app-switches',
@@ -6,11 +13,44 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
+
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+
+  switchesForm: FormGroup = this.formBuilder.group({
+    gender: [genders.male, Validators.required],
+    notifications: [false, Validators.required],
+    termsAndConditions: [ false, Validators.requiredTrue]
+  })
+
+
+  person = {
+    gender: "F",
+    notifications: true,
+  }
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.reset();
+  }
+
+ 
+
+  save(){
+    if(this.switchesForm.invalid)return;
+    const { gender, notifications} = this.switchesForm.value
+    this.person = {gender, notifications}
+    console.log("Person data sent", this.switchesForm.value);
+    this.reset();
+  }
+
+
+  reset(){
+    this.switchesForm.reset({
+      ...this.person,
+      termsAndConditions: false
+    })
   }
 
 }
